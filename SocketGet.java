4c9f320bd55f03c0aef955533eb3cd89b6c3c8a6
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-10-17 18:19:11 -0700

    HTTP/1.1 200 OK
    ...but output returned looks weird:
    
    c2
    ���� E�a�h�k
                �-H��J;����}�۹gr�Q
                                   Z:0����h��8����XaSOГbu3cP+sb����r��q���y�!xcB
    ����.I�Lh)�Ծ��,��t^��yH֐O�b��
    &k8���+V���m�s\:�$ ������b�#���f
                                    ;�+
    0

--- SocketGet.java
+++ SocketGet.java
@@ -11,7 +11,7 @@ class SocketGet
         {
             String urlString = args[0];
             URL httpUrl = new URL(urlString);
-            portNum = 80
+            int portNum = 80;
 
             // TODO: open the socket below
             /* Useful links:
@@ -21,7 +21,7 @@ class SocketGet
              */
             // HINT: you need to create a new Socket object and use httpUrl to get some parameters for the constructor
             // Socket httpSocket = null;
-            Socket httpSocket = Socket(InetAddress httpUrl, int portNum)
+            Socket httpSocket = new Socket(urlString, portNum);
 
             // open up streams to write to and read from
             PrintWriter request = new PrintWriter(httpSocket.getOutputStream(), true);
@@ -34,7 +34,14 @@ class SocketGet
              * https://msdn.microsoft.com/en-us/library/aa287673%28v=vs.71%29.aspx?f=255&MSPPError=-2147217396 
              * 
              */
-            String httpHeader = "GET / HTTP/1.1\r\n                      - COMPLETE THIS GET QUERY";
+            // String httpHeader = "GET / HTTP/1.1\r\n                      - COMPLETE THIS GET QUERY";
+
+            // HTTP/1.1 400 Bad Request
+            // String httpHeader = "GET /index.html HTTP/1.1\r\nHost: http://www.williammortl.com\r\nUser-Agent: Firefox/3.6.10\r\nAccept: text/html,application/xhtml+xml\r\nAccept-Language: en-us,en;q=0.5\r\nAccept-Encoding: gzip,deflate\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n";
+
+
+            // HTTP/1.1 200 OK....but symbols returned are werid looking: ���� E�a�h
+            String httpHeader = "GET /index.html HTTP/1.1\r\nHost: www.williammortl.com\r\nUser-Agent: Firefox/3.6.10\r\nAccept: text/html,application/xhtml+xml\r\nAccept-Language: en-us,en;q=0.5\r\nAccept-Encoding: gzip,deflate\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7\r\nKeep-Alive: 115\r\nConnection: keep-alive\r\n\r\n";
 
             // send the HTTP request
             request.println(httpHeader);
@@ -48,6 +55,7 @@ class SocketGet
             } 
 
             // TODO: close the socket
+            httpSocket.close();
         } 
         catch (UnknownHostException e) 
         {
@@ -59,3 +67,4 @@ class SocketGet
         }
     }
 }
+
